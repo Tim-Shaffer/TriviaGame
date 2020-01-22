a569af74b863e330c174e191638eec7e9c5e73fc
Tim-Shaffer <tcshaf24@ptd.net>
2020-01-22 18:25:35 -0500

    determined some of the basic functionality with buttons and html sections

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,12 +4,27 @@
 // function to initially load the game when start button is clicked or reload when the restart is clicked
 // --------------------------------------------------------------------------------------
 function reloadGame() {
-    
+    // hide the sections that are not needed for the startup
+    $(".question_answer").hide();
+    $(".correct_wrong").hide();
 
 };
 // --------------------------------------------------------------------------------------
 // end of the reloadGame() function
 // --------------------------------------------------------------------------------------
+
+function displayQuestion() {
+    
+    $(".question_answer").show(); 
+
+}
+
+function displayAnswer() {
+     
+    $(".question_answer").hide();   
+    $(".correct_wrong").show();
+
+}
 
 // --------------------------------------------------------------------------------------
 // This function enacts when the html document has been loaded and is ready
@@ -17,9 +32,19 @@ function reloadGame() {
 $(document).ready(function() {
 
     $('body').on('click', '#start', function () { 
-        console.log("Start button Clicked");
+        
+        $(".start").hide();   
+        displayQuestion();  
 
     });
+
+    $('body').on('click', '.answers', function () { 
+       
+        displayAnswer();
+        
+    });
+
+    reloadGame();
 
 
 });
