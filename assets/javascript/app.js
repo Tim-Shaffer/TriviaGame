fc704b1c856c456cd337ad3169f1378fd9739fd2
Tim-Shaffer <tcshaf24@ptd.net>
2020-01-22 14:21:03 -0500

    initial styling with a functioning start button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,28 @@
-// JavaScript and jQuery will be added her
+// JavaScript and jQuery will be added here
+
+// --------------------------------------------------------------------------------------
+// function to initially load the game when start button is clicked or reload when the restart is clicked
+// --------------------------------------------------------------------------------------
+function reloadGame() {
+    
+
+};
+// --------------------------------------------------------------------------------------
+// end of the reloadGame() function
+// --------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------
+// This function enacts when the html document has been loaded and is ready
+// --------------------------------------------------------------------------------------
+$(document).ready(function() {
+
+    $('body').on('click', '#start', function () { 
+        console.log("Start button Clicked");
+
+    });
+
+
+});
+// --------------------------------------------------------------------------------------
+// end of $(document).ready(function()
+// --------------------------------------------------------------------------------------
