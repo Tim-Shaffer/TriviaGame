6073e3c130676dfb6acbc0c69b229b8ed503f88f
Tim-Shaffer <tcshaf24@ptd.net>
2020-01-22 18:55:00 -0500

    added timing to make sure page flowed from one display to the next at certain intervals

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,7 @@
 // JavaScript and jQuery will be added here
 // Variable showAnswer will hold the setInterval for time to show the Answer
 var showAnswer;
+var showQuestion;
 
 // --------------------------------------------------------------------------------------
 // function to initially load the game when start button is clicked or reload when the restart is clicked
@@ -8,7 +9,7 @@ var showAnswer;
 function reloadGame() {
     // hide the sections that are not needed for the startup
     $("#restart").hide();
-    $(".question_answer").hide();
+    $(".question-section").hide();
     $(".answer-section").hide();
 
 };
@@ -17,18 +18,26 @@ function reloadGame() {
 // --------------------------------------------------------------------------------------
 
 function displayQuestion() {
-    
+
+    $(".start").hide(); 
+ 
     // Clear the Answer Interval before showing the next question
     clearInterval(showAnswer);
     $(".answer-section").hide();
-    $(".question_answer").show(); 
+
+    // Set the Question Timer Interval to 20 seconds
+    showQuestion = setInterval(displayAnswer, 5000);
+    $(".question-section").show(); 
 
 }
 
 function displayAnswer() {
      
-    $(".question_answer").hide();   
+    $(".question-section").hide();   
     $(".answer-section").show();
+    // Clear the Question Interval before showing the next question
+    clearInterval(showQuestion);
+
     // Allow the Answer Information to display for 5 seconds
     showAnswer = setInterval(displayQuestion, 5000);
 
@@ -41,16 +50,14 @@ $(document).ready(function() {
 
     // This should only ever happen on the start of the initial load of the game
     $('body').on('click', '#start', function () { 
-        
-        $(".start").hide();   
+          
         displayQuestion();  
 
     });
 
     // This should only ever happen on the re-start of the game
     $('body').on('click', '#restart', function () { 
-        
-        $(".start").hide();   
+         
         displayQuestion();  
 
     });
@@ -67,3 +74,11 @@ $(document).ready(function() {
 // --------------------------------------------------------------------------------------
 // end of $(document).ready(function()
 // --------------------------------------------------------------------------------------
+
+// Copied from the stopwatch to incorporate here later on
+// This code will run as soon as the page loads
+// window.onload = function() {
+//     $(".answers").on("click", displayAnswer);
+//     $("#restart").on("click", displayQuestion);
+//     $("#start").on("click", displayQuestion);
+//   };
